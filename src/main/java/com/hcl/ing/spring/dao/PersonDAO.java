77b938d67305022043f9cd0923eaccd4ac7c36e4
ShlendraSingh <shilu1993sr@gmail.com>
2019-12-13 06:15:24 +0530

    first commit

---
+++ src/main/java/com/hcl/ing/spring/dao/PersonDAO.java
@@ -0,0 +1,18 @@
+package com.hcl.ing.spring.dao;
+
+import java.util.List;
+
+import com.hcl.ing.spring.entity.Person;
+
+public interface PersonDAO {
+
+	public void addPerson(Person p);
+
+	public void updatePerson(Person p);
+
+	public List<Person> listPersons();
+
+	public Person getPersonById(int id);
+
+	public void removePerson(int id);
+}
